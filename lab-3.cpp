d2dfa2a841121b4f4f57808f0fea9e4fee967c0e
bloodypierro <internetwarningg@gmail.com>
2025-10-18 13:04:04 +0200

    Update lab-3.cpp

--- lab-3.cpp
+++ lab-3.cpp
@@ -1,67 +1,77 @@
-//Вариант 4
-//Найти сумму всех отрицательных чисел, наибольшее из таких чисел и количество его повторений.
-//Найти наибольшую цифру числа.
-
-#include <iostream>
+#include
 using namespace std;
 
 int main()
 {
-    // Пункт 1 
-    int amount;
-    cout << "Количество чисел: "; //1) последовательность целочисленных чисел, ввод с  клавиатуры 
-    cin >> amount;     //1)
+// Пункт 1
+int amount;
+cout << "Количество чисел: ";
+cin >> amount;
 
-    int sum = 0, count = 0, maxNeg = 0;
-    bool found = false;
+int sum = 0, count = 0, maxNeg = 0;
+bool found = false;
 
-    cout << "Введите " << amount << " чисел: ";
+cout << "Введите " << amount << " чисел: ";
 
-    for (int i = 0; i < amount; i++) { // последовательность 1)
-        int num;
-        cin >> num;
+for (int i = 0; i < amount; i++) {
+    int num;
+    cin >> num;
 
-        if (num < 0) {
+    if (num < 0) {
+        if (!found) {  // Первое отрицательное число
+            
+            maxNeg = num;
             found = true;
-            sum += num;
-            if (num > maxNeg) {
-                maxNeg = num; //  первое отрицательное
+            count = 1;
+        } else {
+           
+            if (num > maxNeg) {   // Сравниваем с текущим максимумом
+                maxNeg = num;
                 count = 1;
             }
             else if (num == maxNeg) {
                 count++;
             }
         }
+        sum += num;
     }
+}
 
-    if (!found) { // если нету отриц чисел
-        cout << "Нет отрицательных чисел" << endl;
-    }
-    else {
-        cout << "Сумма отрицательных:" << sum << endl;
-        cout << "Наибольшее отрицательное:" << maxNeg << endl;
-        cout << "Его количество:" << count << endl;
-    }
+if (!found) {
+    cout << "Нет отрицательных чисел" << endl;
+}
+else {
+    cout << "Сумма отрицательных: " << sum << endl;
+    cout << "Наибольшее отрицательное: " << maxNeg << endl;
+    cout << "Его количество: " << count << endl;
+}
 
-    // Пункт 2
-    int x; // ПУНКТ 2 ВВОДИМ X 
+// Пункт 2
+int x;
+cout << "Введите число для поиска наибольшей цифры: ";
+cin >> x;
 
-    cin >> x;
+if (x >= -999 && x <= 999) {
+    if (x < 0) x = -x;
+
+    int t = x;
+    int maxnumeral = 0;
     
-    if (x >= -999 && x <= 999) {
-        if (x < 0) x = -x; // убираю минус если есть
-
-       int t = x; // t переменная для работы с числом
-       int maxnumeral = 0;
-       while (t > 0){
-       int numeral = t % 10; // получаем последнюю цирфу
-       if (numeral > maxnumeral){
-       maxnumeral = numeral;
-       }
-       t = t / 10; // удаляем последнюю цифру
-       }
-        cout << "Наибольшая цифра:" << maxnumeral << endl;
+    if (t == 0) {
+        maxnumeral = 0;
+    } else {
+        while (t > 0){
+            int numeral = t % 10;
+            if (numeral > maxnumeral){
+                maxnumeral = numeral;
+            }
+            t = t / 10;
+        }
     }
+    cout << "Наибольшая цифра: " << maxnumeral << endl;
+} else {
+    cout << "Число вне допустимого диапазона" << endl;
+}
 
-    return 0;
+return 0;
 }
